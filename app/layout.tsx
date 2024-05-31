1aeeccbbed1b27f12106096fec4c6b113088e567
wily ramos <wily.ramos1@unmsm.edu.pe>
2024-05-31 11:21:05 -0500

    Parts commons git status

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Footer from "@/components/common/Footer";
+import Header from "@/components/common/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +17,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <>
+      <html lang="en">
+        <body>
+            <header>
+              <div>
+                <Header />
+              </div>
+            </header>
+
+            <main>{children}</main>
+
+            <footer>
+              <Footer />
+            </footer>
+        </body>
+      </html>    
+    </>
+    
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="p-10">
       <h1>Welcome to Fusions App</h1>
     </div>
   );
